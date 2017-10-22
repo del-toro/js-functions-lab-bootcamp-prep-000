595e39e39cab5f3ff2a9dcf82921f3b1b9cc3cac
Brian <deltoro.brian@gmail.com>
2017-10-22 16:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name){
+function happyHolidaysTo("name"){
   return "Happy holidays, ${name}!"
 }
 
-console.log(happyHolidaysTo("Jorb"));
+console.log(happyHolidaysTo(Jorb));
